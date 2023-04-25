a2a41aaefaabaf641b89b7af021940d50bfe7cdc
Mouad Kommir <mouadkommir@gmail.com>
2023-04-25 10:08:39 +0100

    Had some time and made unnecessary corrections

---
+++ README.md
@@ -0,0 +1,4 @@
+#### This repo is very old I don't even know why I created it.
+
+#### After some tweeking, it seems that it tests ltr and rtl modifiers. 
+
--- js/script.js
+++ js/script.js
@@ -4,52 +4,47 @@
  *
  */
 
- $(document).ready(function(){
-    $('#txt1').keypress(pressGenButton());
-});
-
-function pressGenButton() {
-    function(e){
-      if(e.keyCode==13)
-      $('#buttonGenerate').click();
-    }
+function pressGenButton(e) {
+  if(e.keyCode==13)
+    document.getElementById('buttonGenerate').click();
 }
 
- function generate() {
-    var string;
-    var message = document.getElementById("message");
-    var txt1 = document.getElementById("txtMsg1");
-    var txt2 = document.getElementById("txtMsg2");
-    var txt3 = document.getElementById("txtMsg3");
-    var txt4 = document.getElementById("txtMsg4");
+function generate() {
+  var string;
+  var message = document.getElementById("message");
+  var txt1 = document.getElementById("txtMsg1");
+  var txt2 = document.getElementById("txtMsg2");
+  var txt3 = document.getElementById("txtMsg3");
+  var txt4 = document.getElementById("txtMsg4");
 
-    if(txt1.value == '') {
-        txt1.className = 'error';
-    }
-    else {
-        txt1.className = '';
-        string = txt1.value;
-        string += generateChars();
-        string += txt2.value;
-        string += generateChars();
-        string += txt3.value;
-        string += generateChars();
-        string += txt4.value;
-        string += generateChars();
+  if(txt1.value == '') {
+      txt1.className = 'error';
+  }
+  else {
+      txt1.className = '';
+      string = txt1.value;
+      string += generateChars();
+      string += txt2.value;
+      string += generateChars();
+      string += txt3.value;
+      string += generateChars();
+      string += txt4.value;
+      string += generateChars();
 
-        message.innerHTML = string;
-    }
- }
+      message.innerHTML = string;
+  }
+}
 
- function generateChars() {
-    var string = '';
-    for(var i = 0; i < 570; i++) {
-        if(i % 2 == 0) {
-            string += '&lrm';
-        }
-        else {
-            string += '&rlm';
-        }
-    }
-    return string;
- }
+function generateChars() {
+  var string = '';
+  for(var i = 0; i < 570; i++) {
+      if(i % 2 == 0) {
+          string += '&lrm;';
+      }
+      else {
+          string += '&rlm;';
+      }
+  }
+  return string;
+}
+
